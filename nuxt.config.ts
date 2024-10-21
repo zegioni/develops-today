8a545d721603cf0c2a940191e5bfa7d43eb9df47
Mykyta Onishchenko <monishchenko@sporkinc.com>
2024-10-21 21:50:47 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
       NAGER_API_URL: process.env.NAGER_API_URL,
     },
   },
+  app: {
+    baseURL:
+      process.env.NODE_ENV === "production" ? "/<develops-today/>/" : "/",
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@pinia/nuxt"],
