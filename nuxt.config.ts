0442349269cf850d5241b610c782662f5ec621f1
Mykyta Onishchenko <monishchenko@sporkinc.com>
2024-10-21 20:31:21 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    head: {
+      title: "Test",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Test!",
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
+    },
+    baseURL: "/master/",
+  },
   runtimeConfig: {
     public: {
       NAGER_API_URL: process.env.NAGER_API_URL,
