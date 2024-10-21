115cc87a37d7d73ae12eb2d963a8b33050d82786
Mykyta Onishchenko <monishchenko@sporkinc.com>
2024-10-21 21:42:10 +0300

    fix build error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,4 @@
 export default defineNuxtConfig({
-  ssr: false,
-  app: {
-    head: {
-      title: "Test",
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          hid: "description",
-          name: "description",
-          content: "Test!",
-        },
-        { name: "format-detection", content: "telephone=no" },
-      ],
-      link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
-    },
-    baseURL: "/nuxt3-test/",
-  },
   runtimeConfig: {
     public: {
       NAGER_API_URL: process.env.NAGER_API_URL,
@@ -27,8 +9,5 @@ export default defineNuxtConfig({
   modules: ["@nuxt/eslint", "@pinia/nuxt"],
   pinia: {
     storesDirs: ["./stores/**"],
-  },
-  alias: {
-    vue: "vue/dist/vue.esm-bundler.js",
   },
 });
